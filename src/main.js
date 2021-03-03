f39cab9d04cf9c691a6457aa1fc41ab7ca245316
José Diaz <xoxefdp@gmail.com>
2021-03-04 00:16:27 +0100

    Removed --passWithNoTests from test script, create helper and helper spec to move remaining methods from main

--- src/main.js
+++ src/main.js
@@ -3,28 +3,7 @@ import { isNumber, isInteger, isFloat } from 'type/number'
 import { isArray, isEmptyArray } from 'type/array'
 import { isObject, isPlainObject, isEmptyObject } from 'type/object'
 import { isPromise } from 'type/promise'
-
-/**
- * @access public
- * @function isEmpty
- * @description Checks if data is empty, whether is an array or an object.
- * @param {any} data the data to check
- * @returns {boolean} true or false wheter data is an empty objct or empty array
- */
-const isEmpty = (data) => {
-  return isEmptyArray(data) || isEmptyObject(data)
-}
-
-/**
- * @access public
- * @function getType
- * @description Gets data type.
- * @param {any} data the data to check
- * @returns {string} the type to return
- */
-const getType = (data) => {
-  return toString.call(data).replace('[object ', '').replace(']', '')
-}
+import { isEmpty, getType } from 'type/helper'
 
 exports.isNull = isNull
 exports.isUndefined = isUndefined
